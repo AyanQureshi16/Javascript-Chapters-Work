4d64236fd1380638a9c87859f23e083244d55dd5
Ayan Shahid <ayanshahid1612@gmail.com>
2025-07-02 18:44:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// 1st Chapter Done
+
+var num1 =("Error! enter a valid password")
+var num2 =("welcome to js land!\n happy coding")
+var num3 =("Welcome to my website!\nThis is a line break.\nLearning JavaScript is fun!")
+
+
+
+
+alert(num1)
+alert(num2)
+alert(num3)
+
